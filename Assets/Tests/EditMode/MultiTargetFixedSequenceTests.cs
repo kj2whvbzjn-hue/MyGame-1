dcbbee0db499dec48a90ec9bf2daa864079804df
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-09-16 08:35:39 +0900

    Fix FAST RNG fixture for fixed target sequence test

--- Assets/Tests/EditMode/MultiTargetFixedSequenceTests.cs
+++ Assets/Tests/EditMode/MultiTargetFixedSequenceTests.cs
@@ -1,12 +1,14 @@
 #if UNITY_INCLUDE_TESTS
 using NUnit.Framework;
 using GuildAdventure.Game.Battle;
+using GuildAdventure.Game.Core;
 using GuildAdventure.Game.Save;
 namespace GuildAdventure.Tests.EditMode
 {
  public sealed class MultiTargetFixedSequenceTests
  {
-  [Test] public void FixedTargetArray_PreservesDuplicateDrawsAndSequence(){var s=new BattleSnapshotSaveRecord{battleId="B",settingsVersion="S",seed="X"};s.actors.Add(new BattleActorSaveRecord{actorId="A",hp=100,maxHp=100,alive=true});s.actors.Add(new BattleActorSaveRecord{actorId="B",hp=100,maxHp=100,alive=true});s.actors.Add(new BattleActorSaveRecord{actorId="C",hp=100,maxHp=100,alive=true});s.fixedActorOrder.AddRange(new[]{"A","B","C"});var r=new ActionReservationSaveRecord{reservationId="R",actorId="A",skillId="SK",fixedTargetIds=new System.Collections.Generic.List<string>{"C","B","C"}};var result=MultiTargetBattleExecutor.Execute(s,new MultiTargetAttackRequest{reservation=r,buildAttack=id=>new BattleAttackProposal{hitCount=1,baseDamage=1,accuracy=999,evasion=0,criticalRatePercent=0}},new ConstantRandomSource(0.99),new ConstantRandomSource(0),new ConstantRandomSource(0.99));Assert.IsTrue(result.ok,result.reason);CollectionAssert.AreEqual(new[]{"C","B","C"},result.executedTargetIds);Assert.AreEqual(98,s.actors[2].hp);Assert.AreEqual(99,s.actors[1].hp);}
+  sealed class Rng:IRandomSource{readonly double value;public Rng(double value){this.value=value;}public double Next01(string purpose)=>value;}
+  [Test] public void FixedTargetArray_PreservesDuplicateDrawsAndSequence(){var s=new BattleSnapshotSaveRecord{battleId="B",settingsVersion="S",seed="X"};s.actors.Add(new BattleActorSaveRecord{actorId="A",hp=100,maxHp=100,alive=true});s.actors.Add(new BattleActorSaveRecord{actorId="B",hp=100,maxHp=100,alive=true});s.actors.Add(new BattleActorSaveRecord{actorId="C",hp=100,maxHp=100,alive=true});s.fixedActorOrder.AddRange(new[]{"A","B","C"});var r=new ActionReservationSaveRecord{reservationId="R",actorId="A",skillId="SK",fixedTargetIds=new System.Collections.Generic.List<string>{"C","B","C"}};var result=MultiTargetBattleExecutor.Execute(s,new MultiTargetAttackRequest{reservation=r,buildAttack=id=>new BattleAttackProposal{hitCount=1,baseDamage=1,accuracy=999,evasion=0,criticalRatePercent=0}},new Rng(0.99),new Rng(0),new Rng(0.99));Assert.IsTrue(result.ok,result.reason);CollectionAssert.AreEqual(new[]{"C","B","C"},result.executedTargetIds);Assert.AreEqual(98,s.actors[2].hp);Assert.AreEqual(99,s.actors[1].hp);}
  }
 }
 #endif
