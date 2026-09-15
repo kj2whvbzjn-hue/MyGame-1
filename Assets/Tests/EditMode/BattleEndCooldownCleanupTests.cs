b40cecd77ab2303edccdb7b3dd67a7a26c85c648
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-09-16 00:09:44 +0900

    Lock GS-19 battle end cooldown reset contract

--- Assets/Tests/EditMode/BattleEndCooldownCleanupTests.cs
+++ Assets/Tests/EditMode/BattleEndCooldownCleanupTests.cs
@@ -6,7 +6,7 @@ namespace GuildAdventure.Tests.EditMode
 {
  public sealed class BattleEndCooldownCleanupTests
  {
-  [Test] public void CleanupBattleEnd_RemovesNormalCooldownButPreservesPermanentPassiveCooldown(){var s=new BattleSnapshotSaveRecord();var a=new BattleActorSaveRecord{actorId="A"};a.cooldowns.Add(new CooldownSaveRecord{skillId="ACTIVE",remainingTicks=12});a.cooldowns.Add(new CooldownSaveRecord{skillId="FATAL_PASSIVE",remainingTicks=EffectLifecycleRuntime.PermanentBattleCooldown});a.appliedEffects.Add(new AppliedEffectSaveRecord{instanceId="E",effectId="BUFF",kind=EffectLifecycleKind.BUFF.ToString(),remainingTicks=5});a.cast=new CastSaveRecord{active=true,remainingTicks=2};s.actors.Add(a);EffectLifecycleRuntime.CleanupBattleEnd(s);Assert.AreEqual(1,a.cooldowns.Count);Assert.AreEqual("FATAL_PASSIVE",a.cooldowns[0].skillId);Assert.AreEqual(EffectLifecycleRuntime.PermanentBattleCooldown,a.cooldowns[0].remainingTicks);Assert.AreEqual(0,a.appliedEffects.Count);Assert.IsNull(a.cast);}
+  [Test] public void CleanupBattleEnd_ClearsNormalAndPermanentBattleCooldowns(){var s=new BattleSnapshotSaveRecord();var a=new BattleActorSaveRecord{actorId="A"};a.cooldowns.Add(new CooldownSaveRecord{skillId="ACTIVE",remainingTicks=12});a.cooldowns.Add(new CooldownSaveRecord{skillId="FATAL_PASSIVE",remainingTicks=EffectLifecycleRuntime.PermanentBattleCooldown});a.appliedEffects.Add(new AppliedEffectSaveRecord{instanceId="E",effectId="BUFF",kind=EffectLifecycleKind.BUFF.ToString(),remainingTicks=5});a.cast=new CastSaveRecord{active=true,remainingTicks=2};s.actors.Add(a);EffectLifecycleRuntime.CleanupBattleEnd(s);Assert.AreEqual(0,a.cooldowns.Count);Assert.AreEqual(0,a.appliedEffects.Count);Assert.IsNull(a.cast);}
  }
 }
 #endif
