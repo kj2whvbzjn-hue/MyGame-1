4d6847c1c890428cf9a0571524245c80d0d48826
kj2whvbzjn-hue <kj2whvbzjn@privaterelay.appleid.com>
2026-09-15 20:19:43 +0900

    Update trigger lifecycle tests for GS-20

--- Assets/Tests/EditMode/BattleEffectLifecycleTests.cs
+++ Assets/Tests/EditMode/BattleEffectLifecycleTests.cs
@@ -8,27 +8,38 @@ namespace GuildAdventure.Tests.EditMode
 {
     public sealed class BattleEffectLifecycleTests
     {
-        [Test] public void DamageHit_DispatchesOnDamage_InFormalOrder_AndConsumesOnce()
+        [Test] public void DamageHit_DispatchesHitReceivedAndDamageEvents_InFormalOrder()
         {
             var hit=new ResolvedHitSaveRecord{actionId="R",hitIndex=0,sourceId="A",targetId="B",judgement="HIT",actualHpLoss=10};
             var regs=new[]{
-                new TriggerRegistration{id="Y",ownerId="B",trigger=TriggerEvent.ON_DAMAGE,priority=2},
-                new TriggerRegistration{id="X",ownerId="A",trigger=TriggerEvent.ON_DAMAGE,priority=2,once=true}
+                new TriggerRegistration{id="D",ownerId="A",trigger=TriggerEvent.ON_DAMAGE_DEALT,priority=2,sequence=2},
+                new TriggerRegistration{id="H2",ownerId="A",trigger=TriggerEvent.ON_HIT_DEALT,priority=2,sequence=2},
+                new TriggerRegistration{id="H1",ownerId="A",trigger=TriggerEvent.ON_HIT_DEALT,priority=2,sequence=1,once=true},
+                new TriggerRegistration{id="R",ownerId="B",trigger=TriggerEvent.ON_HIT_RECEIVED,priority=1,sequence=1}
             };
-            var order=new Dictionary<string,int>{{"A",0},{"B",1}};
-            var d=BattleEffectLifecycle.DispatchResolvedHit(hit,regs,order);
-            Assert.AreEqual(TriggerEvent.ON_DAMAGE,d.trigger);
-            Assert.AreEqual("X",d.registrations[0].id);
-            Assert.IsTrue(regs[1].consumed);
+            var all=BattleEffectLifecycle.DispatchResolvedHitEvents(hit,regs,new Dictionary<string,int>{{"A",0},{"B",1}});
+            Assert.AreEqual(3,all.Count);
+            Assert.AreEqual(TriggerEvent.ON_HIT_DEALT,all[0].trigger);
+            Assert.AreEqual("H1",all[0].registrations[0].id);
+            Assert.AreEqual("H2",all[0].registrations[1].id);
+            Assert.AreEqual(TriggerEvent.ON_HIT_RECEIVED,all[1].trigger);
+            Assert.AreEqual(TriggerEvent.ON_DAMAGE_DEALT,all[2].trigger);
+            Assert.IsTrue(regs[2].consumed);
         }
 
-        [Test] public void Miss_DispatchesOnHitWithoutDamage()
+        [Test] public void CriticalHit_AlsoDispatchesCriticalBeforeDamage()
+        {
+            var hit=new ResolvedHitSaveRecord{actionId="R",sourceId="A",targetId="B",judgement="CRITICAL",actualHpLoss=4};
+            var all=BattleEffectLifecycle.DispatchResolvedHitEvents(hit,new TriggerRegistration[0],new Dictionary<string,int>());
+            Assert.AreEqual(4,all.Count);
+            Assert.AreEqual(TriggerEvent.ON_CRITICAL,all[2].trigger);
+            Assert.AreEqual(TriggerEvent.ON_DAMAGE_DEALT,all[3].trigger);
+        }
+
+        [Test] public void Miss_DoesNotDispatchOnHit()
         {
             var hit=new ResolvedHitSaveRecord{actionId="R",sourceId="A",targetId="B",judgement="MISS",actualHpLoss=0};
-            var regs=new[]{new TriggerRegistration{id="T",ownerId="A",trigger=TriggerEvent.ON_HIT}};
-            var d=BattleEffectLifecycle.DispatchResolvedHit(hit,regs,new Dictionary<string,int>{{"A",0}});
-            Assert.AreEqual(TriggerEvent.ON_HIT,d.trigger);
-            Assert.AreEqual(1,d.registrations.Count);
+            Assert.AreEqual(0,BattleEffectLifecycle.DispatchResolvedHitEvents(hit,null,new Dictionary<string,int>()).Count);
         }
 
         [Test] public void EffectTick_RemovesExpiredAndConsumedEffects()
